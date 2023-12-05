3861a827f0d56ba352bb24898257d0634e08093e
erba <92913666+boluerbol@users.noreply.github.com>
2023-12-05 10:55:13 +0600

    Update bycamera.py
    I add a price for food 
    example banan - 0.5$

--- new/bycamera.py
+++ new/bycamera.py
@@ -1,16 +1,24 @@
-# import cv2
 # import numpy as np
+# import cv2
 # from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
 # from tensorflow.keras.preprocessing import image
 
-# # Load the pre-trained InceptionV3 model
+# # Load the pre-trained InceptionV3 model only once
 # model = InceptionV3(weights='imagenet')
 
-# def predict_food_from_frame(frame):
+# # Dictionary of food items and their prices
+# food_prices = {
+#     "banana": 0.50,
+#     "apple": 0.60,
+#     "orange": 0.70,
+#     # Add more food items and their prices here
+# }
+
+# def predict_food(image_path, confidence_threshold=0.5):
 #     try:
-#         # Resize the frame to the required input shape for InceptionV3 (299x299)
-#         frame_resized = cv2.resize(frame, (299, 299))
-#         x = image.img_to_array(frame_resized)
+#         # Load the image and resize it to the required input shape for InceptionV3 (299x299)
+#         img = image.load_img(image_path, target_size=(299, 299))
+#         x = image.img_to_array(img)
 #         x = np.expand_dims(x, axis=0)
 #         x = preprocess_input(x)
 
@@ -18,77 +26,22 @@
 #         preds = model.predict(x)
 #         decoded_preds = decode_predictions(preds, top=3)[0]  # Get the top 3 predictions
 
-#         # Print the top predictions
-#         print("Predictions:")
+#         # Print the top predictions above the confidence threshold and their prices
+#         print("Predictions with Prices:")
 #         for _, category, confidence in decoded_preds:
-#             print(f"{category}: {confidence:.2f}")
+#             if confidence >= confidence_threshold and category.lower() in food_prices:
+#                 price = food_prices[category.lower()]
+#                 print(f"{category}: {confidence:.2f} - Price: ${price:.2f}")
 
-#         # Display the frame
-#         cv2.imshow('Camera', frame)
-#         cv2.waitKey(1)  # Keep the window open
+#         # Show the image
+#         img = cv2.imread(image_path)
+#         cv2.imshow('Image', img)
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
 
 #     except Exception as e:
-#         print(f"Error: {e}. Please check the camera feed.")
+#         print(f"Error: {e}. Please check the image path or format.")
 
-# # Open the default camera (usually 0)
-# cap = cv2.VideoCapture(0)
-
-# while True:
-#     ret, frame = cap.read()  # Read a frame from the camera
-#     if not ret:
-#         break
-    
-#     predict_food_from_frame(frame)
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-
-import cv2
-import numpy as np
-from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
-from tensorflow.keras.preprocessing import image
-
-# Load the pre-trained InceptionV3 model
-model = InceptionV3(weights='imagenet')
-
-def predict_food_from_frame(frame, confidence_threshold=0.7):
-    try:
-        # Resize the frame to the required input shape for InceptionV3 (299x299)
-        frame_resized = cv2.resize(frame, (299, 299))
-        x = image.img_to_array(frame_resized)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-
-        # Make predictions
-        preds = model.predict(x)
-        decoded_preds = decode_predictions(preds, top=3)[0]  # Get the top 3 predictions
-
-        # Print the top predictions
-        print("Predictions:")
-        for _, category, confidence in decoded_preds:
-            print(f"{category}: {confidence:.2f}")
-            if confidence >= confidence_threshold and "food" in category.lower():
-                return True
-
-        # Display the frame
-        cv2.imshow('Camera', frame)
-        cv2.waitKey(1)  # Keep the window open
-
-    except Exception as e:
-        print(f"Error: {e}. Please check the camera feed.")
-
-# Open the default camera (usually 0)
-cap = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()  # Read a frame from the camera
-    if not ret:
-        break
-    
-    food_detected = predict_food_from_frame(frame)
-    if food_detected:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# # Replace 'path/to/your/image.jpg' with the path to your image
+# image_path = 'banan.jpg'
+# predict_food(image_path)
